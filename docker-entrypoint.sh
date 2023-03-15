afdca8166ce4d77196d884ee0b14f3602aeca777
You Ming <youming@coovery.com>
2023-03-15 16:57:18 +0800

    Update command.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- python launch.py "$@"
+fi
+
 COMMANDLINE_ARGS="--listen --api --nowebui"
 
 exec "$@"
